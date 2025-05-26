dd32688d7c2cddee846a12b071f583bc3e43e5e6
trlily <dukan98765@gmail.com>
2025-05-26 13:53:04 +0900

    Add files via upload

---
+++ dig_5_conv.cpp
@@ -0,0 +1,324 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+void InverseImage(BYTE* Img, BYTE *Out, int W, int H)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Out[i] = 255 - Img[i];
+	}
+}
+void BrightnessAdj(BYTE* Img, BYTE* Out, int W, int H, int Val)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		if (Img[i] + Val > 255)
+		{
+			Out[i] = 255;
+		}
+		else if (Img[i] + Val < 0)
+		{
+			Out[i] = 0;
+		}
+		else 	Out[i] =Img[i] + Val;
+	}
+}
+void ContrastAdj(BYTE* Img, BYTE* Out, int W, int H, double Val)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		if (Img[i] * Val > 255.0)
+		{
+			Out[i] = 255;
+		}
+		else 	Out[i] = (BYTE)(Img[i] * Val);
+	}
+}
+
+void ObtainHistogram(BYTE* Img, int* Histo, int W, int H)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++) {
+		Histo[Img[i]]++;
+	}
+}
+
+void ObtainAHistogram(int* Histo, int* AHisto)
+{
+	for (int i = 0; i < 256; i++) {
+		for (int j = 0; j <= i; j++) {
+			AHisto[i] += Histo[j];
+		}
+	}
+}
+
+void HistogramStretching(BYTE * Img, BYTE * Out, int * Histo, int W, int H)
+{
+	int ImgSize = W * H;
+	BYTE Low, High;
+	for (int i = 0; i < 256; i++) {
+		if (Histo[i] != 0) {
+			Low = i;
+			break;
+		}
+	}
+	for (int i = 255; i >= 0; i--) {
+		if (Histo[i] != 0) {
+			High = i;
+			break;
+		}
+	}
+	for (int i = 0; i < ImgSize; i++) {
+		Out[i] = (BYTE)((Img[i] - Low) / (double)(High - Low) * 255.0);
+	}
+}
+void HistogramEqualization(BYTE* Img, BYTE* Out, int* AHisto, int W, int H)
+{
+	int ImgSize = W * H;
+	int Nt = W * H, Gmax = 255;
+	double Ratio = Gmax / (double)Nt;
+	BYTE NormSum[256];
+	for (int i = 0; i < 256; i++) {
+		NormSum[i] = (BYTE)(Ratio * AHisto[i]);
+	}
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Out[i] = NormSum[Img[i]];
+	}
+}
+
+void Binarization(BYTE * Img, BYTE * Out, int W, int H, BYTE Threshold)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++) {
+		if (Img[i] < Threshold) Out[i] = 0;
+		else Out[i] = 255;
+	}
+}
+
+//int GozalezBinThresh()
+//{
+//
+//}
+
+void AverageConv(BYTE* Img, BYTE* Out, int W, int H) // ¹Ú½ºÆòÈ°È­
+{
+	double Kernel[3][3] = {0.11111, 0.11111, 0.11111,
+										0.11111, 0.11111, 0.11111,
+										0.11111, 0.11111, 0.11111	};
+	double SumProduct = 0.0;
+	for (int i = 1; i < H-1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W-1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i+m)*W + (j+n)] * Kernel[m+1][n+1];
+				}
+			}
+			Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void GaussAvrConv(BYTE* Img, BYTE* Out, int W, int H) // °¡¿ì½Ã¾ÈÆòÈ°È­
+{
+	double Kernel[3][3] = {0.0625, 0.125, 0.0625,
+										0.125, 0.25, 0.125,
+										0.0625, 0.125, 0.0625 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Prewitt_X_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt ¸¶½ºÅ© X
+{
+	double Kernel[3][3] = { -1.0, 0.0, 1.0,
+										-1.0, 0.0, 1.0,
+										-1.0, 0.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 765  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 3;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Prewitt_Y_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt ¸¶½ºÅ© X
+{
+	double Kernel[3][3] = { -1.0, -1.0, -1.0,
+										0.0, 0.0, 0.0,
+										1.0, 1.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 765  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 3;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Sobel_X_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt ¸¶½ºÅ© X
+{
+	double Kernel[3][3] = { -1.0, 0.0, 1.0,
+										-2.0, 0.0, 2.0,
+										-1.0, 0.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 1020  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 4;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Sobel_Y_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt ¸¶½ºÅ© X
+{
+	double Kernel[3][3] = { -1.0, -2.0, -1.0,
+										0.0, 0.0, 0.0,
+										1.0, 2.0, 1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 765  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 4;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Laplace_Conv(BYTE* Img, BYTE* Out, int W, int H) // Prewitt ¸¶½ºÅ© X
+{
+	double Kernel[3][3] = { -1.0, -1.0, -1.0,
+										-1.0, 8.0, -1.0,
+										-1.0, -1.0, -1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			// 0 ~ 2040  =====> 0 ~ 255
+			Out[i * W + j] = abs((long)SumProduct) / 8;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+void Laplace_Conv_DC(BYTE* Img, BYTE* Out, int W, int H) // Prewitt ¸¶½ºÅ© X
+{
+	double Kernel[3][3] = { -1.0, -1.0, -1.0,
+										-1.0, 9.0, -1.0,
+										-1.0, -1.0, -1.0 };
+	double SumProduct = 0.0;
+	for (int i = 1; i < H - 1; i++) { // YÁÂÇ¥ (Çà)
+		for (int j = 1; j < W - 1; j++) { // XÁÂÇ¥ (¿­)
+			for (int m = -1; m <= 1; m++) { // Kernel Çà
+				for (int n = -1; n <= 1; n++) { // Kernel ¿­
+					SumProduct += Img[(i + m) * W + (j + n)] * Kernel[m + 1][n + 1];
+				}
+			}
+			//Out[i * W + j] = abs((long)SumProduct) / 8;
+			if (SumProduct > 255.0) Out[i * W + j] = 255;
+			else if (SumProduct < 0.0) Out[i * W + j] = 0;
+			else Out[i * W + j] = (BYTE)SumProduct;
+			SumProduct = 0.0;
+		}
+	}
+}
+
+int main()
+{
+	BITMAPFILEHEADER hf; // 14¹ÙÀÌÆ®
+	BITMAPINFOHEADER hInfo; // 40¹ÙÀÌÆ®
+	RGBQUAD hRGB[256]; // 1024¹ÙÀÌÆ®
+	FILE* fp;
+	fp = fopen("lenna.bmp", "rb");
+	if (fp == NULL) {
+		printf("File not found!\n");
+		return -1;
+	}
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
+	int ImgSize = hInfo.biWidth * hInfo.biHeight;
+	BYTE * Image = (BYTE *)malloc(ImgSize);
+	BYTE * Temp = (BYTE*)malloc(ImgSize); // ÀÓ½Ã¹è¿­
+	BYTE* Output = (BYTE*)malloc(ImgSize);
+	fread(Image, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+
+	int Histo[256] = { 0 };
+	int AHisto[256] = { 0 };
+
+	/*ObtainHistogram(Image, Histo, hInfo.biWidth, hInfo.biHeight);
+	ObtainAHistogram(Histo, AHisto);
+	HistogramEqualization(Image, Output, AHisto, hInfo.biWidth, hInfo.biHeight);*/
+	/*int Thres = GozalezBinThresh();
+	Binarization(Image, Output, hInfo.biWidth, hInfo.biHeight, Thres);*/
+
+	//GaussAvrConv(Image, Output, hInfo.biWidth, hInfo.biHeight);
+	
+	/*Sobel_X_Conv(Image, Temp, hInfo.biWidth, hInfo.biHeight);
+	Sobel_Y_Conv(Image, Output, hInfo.biWidth, hInfo.biHeight);
+	for (int i = 0; i < ImgSize; i++) {
+		if (Temp[i] > Output[i]) 	Output[i] = Temp[i];
+	}
+	Binarization(Output, Output, hInfo.biWidth, hInfo.biHeight, 40);*/
+	GaussAvrConv(Image, Temp, hInfo.biWidth, hInfo.biHeight);
+	Laplace_Conv_DC(Temp, Output, hInfo.biWidth, hInfo.biHeight);
+	
+	//HistogramStretching(Image, Output, Histo, hInfo.biWidth, hInfo.biHeight);
+	//InverseImage(Image, Output, hInfo.biWidth, hInfo.biHeight);
+	//BrightnessAdj(Image, Output, hInfo.biWidth, hInfo.biHeight, 70);
+	//ContrastAdj(Image, Output, hInfo.biWidth, hInfo.biHeight, 0.5);
+
+	fp = fopen("output.bmp", "wb");
+	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+	fwrite(Output, sizeof(BYTE), ImgSize, fp);
+	fclose(fp);
+	free(Image);
+	free(Output);
+	free(Temp);
+	return 0;
+}
